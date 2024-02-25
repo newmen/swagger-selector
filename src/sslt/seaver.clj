70242d65fd39ed57c7342d85ad0215bf9b6eafc6
Gleb Averchuk <altermn@gmail.com>
2024-02-25 23:56:02 +0300

    split seaver into smaller functions

--- src/sslt/seaver.clj
+++ src/sslt/seaver.clj
@@ -1,43 +1,54 @@
 (ns sslt.seaver
   (:require [sslt.selector :as s]))
 
+(defn methods-builder
+  [path path-data acc method]
+  (if-let [data (path-data (name method))]
+    (assoc acc (name method) data)
+    (throw (ex-info (str "Undefined method")
+                    {:path path
+                     :method method}))))
+
+(defn paths-builder
+  [paths acc path methods]
+  (if-let [path-data (paths path)]
+    (assoc acc path
+           (reduce (partial methods-builder path path-data)
+                   (sorted-map)
+                   methods))
+    (throw (ex-info (str "Undefined path")
+                    {:path path
+                     :methods methods}))))
+
+(defn update-paths
+  [paths path-methods]
+  (reduce-kv (partial paths-builder paths)
+             (sorted-map)
+             path-methods))
+
 (defn select-methods
   [sw path-methods]
-  (update sw "paths"
-          (fn [paths]
-            (reduce-kv (fn [acc path methods]
-                         (if-let [path-data (paths path)]
-                           (assoc acc path
-                                  (reduce (fn [a method]
-                                            (if-let [data (path-data (name method))]
-                                              (assoc a (name method) data)
-                                              (throw (ex-info (str "Undefined method")
-                                                              {:path path
-                                                               :method method}))))
-                                          (sorted-map)
-                                          methods))
-                           (throw (ex-info (str "Undefined path")
-                                           {:path path
-                                            :methods methods}))))
-                       (sorted-map)
-                       path-methods))))
+  (update sw "paths" update-paths path-methods))
+
+(defn build-type
+  [schemas acc type-name]
+  (if-let [type-data (schemas type-name)]
+    (assoc acc type-name type-data)
+    (throw (ex-info (str "Undefined type")
+                    {:type type-name}))))
+
+(defn update-schemas
+  [schemas cut-type-names]
+  (reduce (partial build-type schemas)
+          {}
+          cut-type-names))
 
 (defn select-types
   [sw types]
   (let [cut-type-names (map (comp last s/split-component-path) types)]
     (-> sw
-        (update "components"
-                (fn [components]
-                  (dissoc components "securitySchemes")))
-        (update-in ["components" "schemas"]
-                   (fn [schema-types]
-                     (reduce (fn [acc type-name]
-                               (if-let [type-data (schema-types type-name)]
-                                 (assoc acc type-name type-data)
-                                 (throw (ex-info (str "Undefined type")
-                                                 {:type type-name}))))
-                             {}
-                             cut-type-names))))))
+        ;; (update "components" dissoc "securitySchemes")
+        (update-in ["components" "schemas"] update-schemas cut-type-names))))
 
 (defn seave-methods
   [sw path-methods]
@@ -45,4 +56,5 @@
     (-> sw
         (select-methods path-methods)
         (select-types types)
-        (dissoc "info" "tags" "servers" "security"))))
+        ;; (dissoc "info" "tags" "servers" "security")
+        )))
